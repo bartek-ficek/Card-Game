d093558bd0c53fa29164a43183f67dfd77e73551
Bartek Ficek <ficek.bartlomiej@gmail.com>
2020-09-12 13:57:44 +0200

    initial state of the homework

--- src/main/java/isa/jjdd/Homeworks.java
+++ src/main/java/isa/jjdd/Homeworks.java
@@ -2,7 +2,7 @@ package isa.jjdd;
 
 public class Homeworks {
     public static void main(String[] args) {
-        // TODO: Create comparator and deck objects
+        // TODO: Create comparator and deck objects BF start
         SomeRandomCardGame game = new SomeRandomCardGame(null, null);
         game.startGame();
     }
