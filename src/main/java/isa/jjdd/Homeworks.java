bd10442cb05dea70a1dff1c799550c2701c5291c
Bartek Ficek <ficek.bartlomiej@gmail.com>
2020-09-14 07:16:17 +0200

    Lost in building class Card

--- src/main/java/isa/jjdd/Homeworks.java
+++ src/main/java/isa/jjdd/Homeworks.java
@@ -1,9 +1,25 @@
 package isa.jjdd;
 
+import isa.jjdd.models.Card;
+import isa.jjdd.models.CardTry;
+
 public class Homeworks {
     public static void main(String[] args) {
         // TODO: Create comparator and deck objects BF start
-        SomeRandomCardGame game = new SomeRandomCardGame(null, null);
-        game.startGame();
+//        SomeRandomCardGame game = new SomeRandomCardGame(null, null);
+//        game.startGame();
+
+        Card.Spades spades = new Card().new Spades();
+        System.out.println(spades.toString());
+
+        Card.Spades.Two twoOfSpades = spades.new Two();
+        System.out.println(twoOfSpades.toString());
+
+        Card.Spades.Three threeOfSpades = spades.new Three();
+        System.out.println(threeOfSpades.toString());
+
+        Card<Card.Spades, Card.Two> tumturu = new Card<>(new Card);
+
+
     }
 }
--- src/main/java/isa/jjdd/models/Card.java
+++ src/main/java/isa/jjdd/models/Card.java
@@ -1,5 +1,39 @@
 package isa.jjdd.models;
 
-public class Card {
-    // TODO: implement Card class
+public class Card <T extends Card, E extends Card.Spades> {
+    //TODO Im not sure <T> above in class card. I added, but only to test.
+    public String suit;
+    public int rank;
+
+    @Override
+    public String toString() {
+        return rank + " of " + suit;
+    }
+    //    public Card() {
+//        this.suit = null;
+//        this.rank = 0;
+//    }
+    //TODO Do I need constructor above?
+
+    public class Spades extends Card {
+        public Spades() {
+            suit = "Spades";
+        }
+    }
+    public class Two extends Spades {
+        public Two() {
+            super(suit);
+            rank = 2;
+        }
+    }
+    public class Three extends Spades {
+        public Three() {
+            super();
+            rank = 3;
+        }
+    }
+
 }
+
+// TODO: implement Card class
+
